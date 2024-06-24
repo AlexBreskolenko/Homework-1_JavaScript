3d2c1e68ec0e468c0c1e8ae67541f328b6c290e8
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-06-24 13:23:24 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 let degreesCelsius = 21;
 let degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
 console.log(`Температура в фаренгейтах = ${degreesFahrenheit}`);
+let userName = 'Alex';
+let admin = userName;
+console.log(admin);
